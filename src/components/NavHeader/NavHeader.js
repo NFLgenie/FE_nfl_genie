88490d30639e22971737afb60857ec099f1a15e7
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-19 15:48:42 -0600

    Add buttons

--- src/components/NavHeader/NavHeader.js
+++ src/components/NavHeader/NavHeader.js
@@ -15,7 +15,9 @@ const NavHeader = () => {
       <select className='week-drop-box'>
         { buildWeeks().map(week => <option key={week} >week {week}</option>) }
       </select>
+      <p>userName</p>
       <button className='login-button'>log in</button>
+      <button className='save-button'>save</button>
     </section>
   )
 
