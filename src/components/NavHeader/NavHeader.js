43656a7316ac3f5eb938fb8b8e094375692e3508
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-19 15:39:02 -0600

    Build weeks 1-17 in dropdown

--- src/components/NavHeader/NavHeader.js
+++ src/components/NavHeader/NavHeader.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './NavHeader.css';
 
 const NavHeader = () => {
 
+  const buildWeeks = () => {
+    let weeks = [];
+    for (let i = 1; i < 18; i++) {weeks.push(i)}
+    return weeks;
+  }
+
   return (
-    <section>
-      <h1>header</h1>
+    <section className='nav-outer-container'>
+      <h1>NFL Genie 🧞‍♂️</h1>
+      <select className='week-drop-box'>
+        { buildWeeks().map(week => <option>week {week}</option>) }
+      </select>
     </section>
   )
 
