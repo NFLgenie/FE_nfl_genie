8b2bc1719e18eeccecf3d27415838ec34e38d8df
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-19 16:01:49 -0600

    Create component function

---
+++ src/components/MatchUp/MatchUp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MatchUp.css';
+
+const MatchUp = () => {
+
+  return(
+    <section>
+      <h1>matchUp thing</h1>
+    </section>
+  )
+}
+
+export default MatchUp;
