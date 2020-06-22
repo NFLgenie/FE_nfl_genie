3e3ec52b7e2c06efb2419d9c615c5480c16b298e
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-22 15:58:58 -0600

    Build basic skeleton

---
+++ src/components/CurrentRecord/CurrentRecord.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CurrentRecord.css';
+
+const CurrentRecord = () => {
+
+  return(
+    <section className='current-record-outer-container'>
+      <h1>current record box</h1>
+    </section>
+  )
+}
+
+export default CurrentRecord;
