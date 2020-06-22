e891def1136d8df6fa84bd9aae82f489de9215f2
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-22 16:41:12 -0600

    Create container div

--- src/components/CurrentRecord/CurrentRecord.js
+++ src/components/CurrentRecord/CurrentRecord.js
@@ -5,7 +5,17 @@ const CurrentRecord = () => {
 
   return(
     <section className='current-record-outer-container'>
-      <h1>current record box</h1>
+      <h1 className='title'>AFC</h1>
+      <p className='heading-labels'>rank</p>
+      <p className='heading-labels'>w-l-t</p>
+      <p className='heading-labels'>conf</p>
+      <hr></hr>
+
+      <div className='singe-record-container'>
+      <button className='team-record-logo-button'>
+      <img src={require(`../../assets/teamLogos/CHI.svg`)} className='team-record-logo'/>
+      CHI</button>
+      
     </section>
   )
 }
