eb37599f553a823f7e95615fc52200613e49462f
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-22 17:18:45 -0600

    Modify dummy data

--- src/components/CurrentRecord/CurrentRecord.js
+++ src/components/CurrentRecord/CurrentRecord.js
@@ -6,36 +6,38 @@ const CurrentRecord = () => {
   return(
     <section className='current-record-outer-container'>
       <h1 className='title'>AFC</h1>
-      <p className='heading-labels'>rank</p>
-      <p className='heading-labels'>w-l-t</p>
-      <p className='heading-labels'>conf</p>
+      <div className='label-container'>
+        <p className='rank-label'>rank</p>
+        <p className='WLT-label'>w-l-t</p>
+        <p className='conf-label'>conf</p>
+      </div>
       <hr></hr>
 
       <div className='singe-record-container'>
-        <p className='rank-label'>1</p>
+        <p className='rank-text'>1</p>
         <button className='team-record-logo-button'>
         <img src={require(`../../assets/teamLogos/CHI.svg`)} className='team-record-logo'/>
         CHI</button>
-        <p className='WLT-labels'>1-5-0</p>
-        <p className='WLT-labels'>1-2-0</p>
+        <p className='WLT-text'>1-5-0</p>
+        <p className='WLT-text'>1-2-0</p>
       </div>
 
       <div className='singe-record-container'>
-        <p className='rank-label'>2</p>
+        <p className='rank-text'>2</p>
         <button className='team-record-logo-button'>
         <img src={require(`../../assets/teamLogos/BAL.svg`)} className='team-record-logo'/>
-        CHI</button>
-        <p className='WLT-labels'>1-2-0</p>
-        <p className='WLT-labels'>2-2-0</p>
+        BAL</button>
+        <p className='WLT-text'>1-2-0</p>
+        <p className='WLT-text'>2-2-0</p>
       </div>
 
       <div className='singe-record-container'>
-        <p className='rank-label'>3</p>
+        <p className='rank-text'>3</p>
         <button className='team-record-logo-button'>
         <img src={require(`../../assets/teamLogos/BUF.svg`)} className='team-record-logo'/>
-        CHI</button>
-        <p className='WLT-labels'>3-4-0</p>
-        <p className='WLT-labels'>3-1-1</p>
+        BUF</button>
+        <p className='WLT-text'>3-4-0</p>
+        <p className='WLT-text'>3-1-1</p>
       </div>
 
     </section>
