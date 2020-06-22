f53c6bb4de59199019176f451e0c945ee6fd4e07
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-22 17:00:49 -0600

    Create 3x dummy data

--- src/components/CurrentRecord/CurrentRecord.js
+++ src/components/CurrentRecord/CurrentRecord.js
@@ -12,10 +12,32 @@ const CurrentRecord = () => {
       <hr></hr>
 
       <div className='singe-record-container'>
-      <button className='team-record-logo-button'>
-      <img src={require(`../../assets/teamLogos/CHI.svg`)} className='team-record-logo'/>
-      CHI</button>
-      
+        <p className='rank-label'>1</p>
+        <button className='team-record-logo-button'>
+        <img src={require(`../../assets/teamLogos/CHI.svg`)} className='team-record-logo'/>
+        CHI</button>
+        <p className='WLT-labels'>1-5-0</p>
+        <p className='WLT-labels'>1-2-0</p>
+      </div>
+
+      <div className='singe-record-container'>
+        <p className='rank-label'>2</p>
+        <button className='team-record-logo-button'>
+        <img src={require(`../../assets/teamLogos/BAL.svg`)} className='team-record-logo'/>
+        CHI</button>
+        <p className='WLT-labels'>1-2-0</p>
+        <p className='WLT-labels'>2-2-0</p>
+      </div>
+
+      <div className='singe-record-container'>
+        <p className='rank-label'>3</p>
+        <button className='team-record-logo-button'>
+        <img src={require(`../../assets/teamLogos/BUF.svg`)} className='team-record-logo'/>
+        CHI</button>
+        <p className='WLT-labels'>3-4-0</p>
+        <p className='WLT-labels'>3-1-1</p>
+      </div>
+
     </section>
   )
 }
