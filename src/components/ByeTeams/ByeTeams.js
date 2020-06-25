2aed2abbd06e75ffdb003a419ae8953add77297d
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-25 13:58:07 -0600

    Add dummy data

--- src/components/ByeTeams/ByeTeams.js
+++ src/components/ByeTeams/ByeTeams.js
@@ -8,9 +8,13 @@ const ByeTeams = () => {
       <h1 className='title'>Teams on Bye Week</h1>
       <hr></hr>
 
-      <div className='team-record-logo-button'>
+      <div className='bye-team-logo'>
       <img src={require(`../../assets/teamLogos/BAL.svg`)} className='team-record-logo'/>
       BAL</div>
+
+      <div className='bye-team-logo'>
+      <img src={require(`../../assets/teamLogos/CIN.svg`)} className='team-record-logo'/>
+      CIN</div>
 
     </section>
   )
