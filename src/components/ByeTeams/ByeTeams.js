6bb5be8548e7f5912070586ef119705e6a75eb7c
Zachary Nemeroff <zanemeroff@gmail.com>
2020-06-25 13:30:21 -0600

    Create basic component skeleton

---
+++ src/components/ByeTeams/ByeTeams.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ByeTeams.css';
+
+const ByeTeams = () => {
+
+
+  return (
+    <section>
+      <h1>Bye teams component</h1>
+    </section>
+  )
+}
+
+export default ByeTeams;
